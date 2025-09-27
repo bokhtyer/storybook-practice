75749965b333c7fa522f62117cbbfac9ef74dfb0
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2025-09-27 11:29:15 +0600

    Update Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,3 +1,7 @@
 export default function Card() {
-    return <div>Hello Card Code Update</div>;
+    return (
+        <div>
+            <h2>Hello Card Code Update Again Update Card</h2>
+        </div>
+    );
 }
