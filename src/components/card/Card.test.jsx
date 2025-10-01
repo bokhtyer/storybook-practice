0526a79a7f29fc7fa2a091c5ebc9140435d6fdaf
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2025-10-01 08:22:44 +0600

    jest snapshot added

--- src/components/card/Card.test.jsx
+++ src/components/card/Card.test.jsx
@@ -3,8 +3,10 @@ import Card from "./Card";
 import "@testing-library/jest-dom";
 
 test("renders Card with title", () => {
-    render(<Card title="My Card Title" content={"Hello content"} alignment="center" />);
-    expect(screen.getByText("My Card Title")).toBeInTheDocument();
+    const { asFragment } = render(<Card title="My Card Title A" content="Hello content" alignment="center" />);
+    expect(screen.getByText("My Card Title A")).toBeInTheDocument();
     expect(screen.getByText("Hello content")).toBeInTheDocument();
     expect(screen.getByTestId("card-item")).toHaveStyle("text-align: center");
+
+    expect(asFragment()).toMatchSnapshot();
 });
